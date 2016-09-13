3606859ef50ee207d0a30a2e1966a2d351fa3ed6
eikiyako <eiki.526pod@icloud.com>
2016-09-13 04:22:40 +0000

    add posting microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+    @micropost = current_user.microposts.build if logged_in?
+    # Micropost.new(user_id: current_user.id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
+   before_action :me?, only: [:edit, :update]
+   before_action :logged_in_user, only: [:show]
   
-  def show # 追加
-   @user = User.find(params[:id])
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.order(created_at: :desc)
   end
   
   def new
@@ -12,9 +15,24 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
        flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user # ここを修正
+      redirect_to @user
     else
       render 'new'
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:info] = 'Profileを更新しました。'
+      redirect_to @user
+    else
+      flash[:danger] = 'Profileの更新に失敗しました。'
+      render 'edit'
     end
   end
 
@@ -22,7 +40,14 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation,:intro)
+  end
+  
+  def me?
+    @user = User.find(params[:id])
+    if current_user != @user
+      redirect_to root_path
+    end
   end
   
 end
