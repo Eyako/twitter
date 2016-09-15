53ec8c8dab9920501f21fce0828711878fa4cb14
eikiyako <eiki.526pod@icloud.com>
2016-09-15 03:29:08 +0000

    add followings/followers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+(1..20).each do |i|
+  u = User.new(name: "test#{i}", email: "test#{i}@example.com")
+  u.password = '111111'
+  u.save!(validate: false)
+end
+
+Relationship.create(follower_id: 1, followed_id: 2)
+Relationship.create(follower_id: 1, followed_id: 3)
+Relationship.create(follower_id: 1, followed_id: 4)
+Relationship.create(follower_id: 2, followed_id: 1)
+Relationship.create(follower_id: 3, followed_id: 1)
+
+# (1..20).each do |i|
+#     u = User.find(i)
+#     u2= User.find(i+1)
+#     u.follow(u2)
+#   end
